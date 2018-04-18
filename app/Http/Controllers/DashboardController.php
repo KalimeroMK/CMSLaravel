5435d0257bf5c95b7cc4c0ab449a9ac3b0ba679d
Zoran <zbogoevski@gmail.com>
2018-04-19 01:13:36 +0200

    update od 5.2 na 5.3

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,163 +2,139 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests;
 use App\User as User;
-use Validator;
-use DB;
+use Hash;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
-use Illuminate\Database\Eloquent\Model;
+use Input;
 use Intervention\Image\ImageManagerStatic as Image;
 use Session;
-use Hash;
-use Input;
-use App\Country as Country;
+use Validator;
 
-class DashboardController extends Controller
-{
+class DashboardController extends Controller {
 
-    use AuthenticatesAndRegistersUsers;
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware('auth');
+	}
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		return view('admin.regular');
+	}
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return view('admin.regular');
-    }
+	public function listusers() {
+		$users = User::orderBy('created_at', 'asc')->paginate(10);
+		$data = ['status' => 'all', 'users' => $users];
+		return view('admin.users')->with($data);
+	}
 
+	public function listuser($id) {
+		$user = User::find($id);
 
-    public function listusers()
-    {
-        $users = User::orderBy('created_at', 'asc')->paginate(10);
-        $data = ['status' => 'all', 'users' => $users];
-        return view('admin.users')->with($data);
-    }
+		$data = ['status' => 'single', 'user' => $user];
 
-    public function listuser($id)
-    {
-        $user = User::find($id);
+		return view('admin.users')->with($data);
+	}
 
-        $data = ['status' => 'single', 'user' => $user];
+	public function create() {
+		return view('admin.adduser');
+	}
 
-        return view('admin.users')->with($data);
-    }
+	public function edituser($id) {
+		$user = User::find($id);
+		$data = ['status' => 'edit', 'user' => $user];
+		return view('admin.edituser')->with($data);
+	}
 
-    public function create()
-    {
-        return view('admin.adduser');
-    }
+	protected function register(Request $data) {
 
-    public function edituser($id)
-    {
-        $user = User::find($id);
-        $data = ['status' => 'edit', 'user' => $user];
-        return view('admin.edituser')->with($data);
-    }
+		$errors = Validator::make($data->all(), [
+			'name' => 'required|max:255',
+			'email' => 'required|email|max:255|unique:users',
+			'password' => 'required|confirmed|min:6',
+			'role' => 'required',
+		]);
 
-    protected function register(Request $data)
-    {
+		if ($errors->fails()) {
+			return redirect('/admin/users/create')
+				->withErrors($errors)
+				->withInput();
+		}
+		$create = User::create([
+			'name' => $data['name'],
+			'email' => $data['email'],
+			'role' => $data['role'],
+			'password' => bcrypt($data['password']),
+		]);
+		return redirect('/admin/users');
+	}
 
+	protected function updateuser(Request $data) {
 
-        $errors = Validator::make($data->all(), [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-            'role' => 'required',
-        ]);
+		$id = $data['id'];
+		$user = User::findOrFail($id);
 
-        if ($errors->fails()) {
-            return redirect('/admin/users/create')
-                ->withErrors($errors)
-                ->withInput();
-        }
-        $create = User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'role' => $data['role'],
-            'password' => bcrypt($data['password']),
-        ]);
-        return redirect('/admin/users');
-    }
+		$errors = Validator::make($data->all(), [
+			'name' => 'required|max:255',
+			'email' => 'required|email|max:255|',
+			'role' => 'required',
+			'password' => 'confirmed|min:6',
+		]);
 
-    protected function updateuser(Request $data)
-    {
+		if ($errors->fails()) {
+			return redirect('/admin/users/' . $id . '/edit')
+				->withErrors($errors)
+				->withInput();
+		}
 
-        $id = $data['id'];
-        $user = User::findOrFail($id);
+		if (trim(Input::get('password')) == '') {
+			$data = Input::except('password', 'password_confirmation');
+		} else {
+			$data = Input::all();
+			$data['password'] = Hash::make($data['password']);
+		}
 
-        $errors = Validator::make($data->all(), [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|',
-            'role' => 'required',
-            'password' => 'confirmed|min:6',
-        ]);
+		$user->fill($data)->save();
 
-        if ($errors->fails()) {
-            return redirect('/admin/users/' . $id . '/edit')
-                ->withErrors($errors)
-                ->withInput();
-        }
+		if (Input::hasFile('image')) {
+			$image = Input::file('image');
+			$path = public_path() . '/assets/img/avatars';
+			$pathThumb = public_path() . '/assets/img/avatars/thumbnails/';
+			$ext = $image->getClientOriginalExtension();
+			$imageName = (rand(11111, 99999) . time()) . '.' . $ext;
 
+			$image->move($path, $imageName);
 
-        if (trim(Input::get('password')) == '') {
-            $data = Input::except('password', 'password_confirmation');
-        } else {
-            $data = Input::all();
-            $data['password'] = Hash::make($data['password']);
-        }
+			$findimage = public_path() . '/assets/img/avatars/' . $imageName;
 
+			$imagethumb = Image::make($findimage)->resize(300, null, function ($constraint) {
+				$constraint->aspectRatio();
+			})->fit(200);
 
-        $user->fill($data)->save();
+			$imagethumb->save($pathThumb . $imageName);
 
-        if (Input::hasFile('image')) {
-            $image = Input::file('image');
-            $path = public_path() . '/assets/img/avatars';
-            $pathThumb = public_path() . '/assets/img/avatars/thumbnails/';
-            $ext = $image->getClientOriginalExtension();
-            $imageName = (rand(11111, 99999) . time()) . '.' . $ext;
+			$user->imagethumb = $imageName;
+			$user->image = $imageName;
+			$user->save();
+		}
 
+		Session::flash('flash_message', 'User successfully edited!');
 
-            $image->move($path, $imageName);
+		return redirect()->back();
+	}
 
-            $findimage = public_path() . '/assets/img/avatars/' . $imageName;
-
-            $imagethumb = Image::make($findimage)->resize(300, null, function ($constraint) {
-                $constraint->aspectRatio();
-            })->fit(200);
-
-
-            $imagethumb->save($pathThumb . $imageName);
-
-
-            $user->imagethumb = $imageName;
-            $user->image = $imageName;
-            $user->save();
-        }
-
-        Session::flash('flash_message', 'User successfully edited!');
-
-        return redirect()->back();
-    }
-
-    protected function delete($id)
-    {
-        $user = User::find($id);
-        $user->delete();
-        return redirect('/admin/users');
-    }
+	protected function delete($id) {
+		$user = User::find($id);
+		$user->delete();
+		return redirect('/admin/users');
+	}
 
 }
--- app/User.php
+++ app/User.php
@@ -3,33 +3,31 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable {
+	use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-    'name', 'email', 'password', 'image','role',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password', 'image', 'role',
+	];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-    'password', 'remember_token',
-    ];
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
 
-
-
-    public function ProductList()
-    {
-        return $this->belongsTo('App\Product','id','user_id')->orderBy('updated_at','desc');
-    }
-
+	public function ProductList() {
+		return $this->belongsTo('App\Product', 'id', 'user_id')->orderBy('updated_at', 'desc');
+	}
 
 }
